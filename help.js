7a5b40d4a0f12a3a77de387190471fac782b2683
Falconbot2 <79382970+Falconbot2@users.noreply.github.com>
2021-03-28 08:37:00 -0300

    Add files via upload

---
+++ help.js
@@ -0,0 +1,160 @@
+const help = (prefix) => { 
+	return `                 
+	
+●▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬●
+ ░░░░░░░░░░░𝐅𝐀𝐋𝐂𝐎𝐍 𝐁𝐎𝐓░░░░░░░░░░░ 
+●▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬●
+
+
+𝚅𝚄𝙻𝙶𝙾: 𝙵𝙰𝙻𝙲𝙾𝙽
+𝙲𝙾𝙽𝚃𝙰𝚃𝙾: wa.me/+5521993471668
+𝚅𝙴𝚁𝚂𝙰̃𝙾: 1.0
+𝙿𝚁𝙴𝙵𝙸𝚇: ${prefix}
+
+
+
+──▒▒▒▒▒────▄████▄─────
+─▒─▄▒─▄▒──███▄█▀──────
+─▒▒▒▒▒▒▒─▐████──█──█──
+─▒▒▒▒▒▒▒──█████▄──────
+─▒─▒─▒─▒───▀████▀─────
+
+
+● 𝙱𝙾𝚃 𝙼𝙴𝙽𝚄 ●
+
+● 𝙰𝚄𝚃𝙷𝙾𝚁 - 𝙵𝙰𝙻𝙲𝙾𝙽 𝙱𝙾𝚃 ●
+
+ 
+
+╔══════════════════════
+║ ᏟϴᎷᎪΝᎠϴՏ ᏞᏆՏͲᎪᎠϴՏ
+╠══════════════════════
+║   ᎬХᏟᏞႮՏᏆᏙϴ ᏢᎪᎡᎪ ᎪᎠᎷ'Տ
+║╭─────────────────────
+║├────> ${prefix}kick @menciona o membro
+║├> banir membro 
+║├─────────────────────
+║├> ${prefix}add +55xxxxxxxxx
+║├────> add menbro
+║├─────────────────────
+║├> ${prefix}listadmin
+║├────> listar cm todos ADM's
+║├─────────────────────
+║├> ${prefix}welcome [1/0]
+║├────> ativar/desativar boas vindas
+║├─────────────────────
+║├> ${prefix}group fechar
+║├────> fecha grupo 
+║├─────────────────────
+║├> ${prefix}group abrir
+║├────> abrir grupo 
+║├─────────────────────
+║├> ${prefix}demote @marca adm
+║├────> retirar ADM de alguém
+║├─────────────────────
+║├> ${prefix}promote @marca membro
+║├────> promover a ADM
+║├─────────────────────
+║├> ${prefix}nsfw [1/0]
+║├────> ativa/desativar modo loli
+║├─────────────────────
+║├> ${prefix}simih [1/0]
+║├────> conversar com o bot
+║└─────────────────────
+╠══════════════════════
+║   ᎷᏆᎠᏆᎪ Ꮖ ՏͲᏆᏟᏦᎬᎡ
+║╭─────────────────────
+║├> ${prefix}sticker + foto/Gif
+║├────> fazer figurinha
+║├─────────────────────
+║├> ${prefix}anjing
+║├────> dog aleatório fofinho
+║├─────────────────────
+║├> ${prefix}text3d seu txt
+║├────> uma foto cm seu txt
+║├─────────────────────
+║├> ${prefix}pokemon
+║├────> um pokemon aleatório
+║├─────────────────────
+║├> ${prefix}ocr (marque uma foto)
+║├────> pegar txts de uma foto
+║├─────────────────────
+║├> ${prefix}tts pt seu txt
+║├────> transformar txt em áudio
+║├─────────────────────
+║├> ${prefix}toimg (marca uma fig)
+║├────> converter fig em foto
+║├─────────────────────
+║├> ${prefix}ytmp4 (link de algum vídeo)
+║├────> pega informações de vídeo
+║├─────────────────────
+║├> ${prefix}hilih seu txt
+║├────> converter todas vogais do
+║│ txt para "i"
+║├─────────────────────
+║├> ${prefix}quotes
+║├────> txt de motivação (indo)
+║├─────────────────────
+║├> ${prefix}map 00000-000
+║├────> pega imagem do local
+║│desejado 
+║├─────────────────────
+║├> ${prefix}nsfwloli
+║├────> foto de loli
+║├─────────────────────
+║├> ${prefix}loli
+║├────> foto de loli
+║├─────────────────────
+║├> ${prefix}fototiktok
+║├────> ??? Sla
+║├─────────────────────
+║├> ${prefix}phlogo seu|txt
+║├────> logo do porn hub cm seu txt
+║├─────────────────────
+║├> ${prefix}tiktokstalk
+║├────> ?? Não sei como funciona
+║└─────────────────────
+╠══════════════════════
+║   ᏟϴᎷᎪΝᎠϴՏ ᏢᎪᎡᎪ ϴ ᏟᎡᏆᎪᎠϴᎡ
+║╭─────────────────────
+║├> ${prefix}clearall
+║├────> sla
+║├─────────────────────
+║├> ${prefix}bloquear
+║├────> bloquear o membro
+║├─────────────────────
+║├> ${prefix}desbloquear
+║├────> desbloquear o membro
+║├─────────────────────
+║├> ${prefix}bc
+║├────> transmissão 
+║├─────────────────────
+║├> ${prefix}leave
+║├────> sla
+║├─────────────────────
+║├> ${prefix}setpp
+║├────> sla
+║├─────────────────────
+║├> ${prefix}setprefix
+║├────> mudar sigla
+║└─────────────────────
+╚═══════════════════════
+
+
+⠄⠄⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄
+⠄⠄⢸⣇⠻⣿⣿⣿⣧⣀⢀⣠⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⠄
+⠄⢀⢸⣿⣷⣤⣤⣤⣬⣙⣛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡍⠄⠄⢀⣤⣄⠉⠋⣰
+⠄⣼⣖⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇⢀⣤
+⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴⣿⡗
+⢀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄
+⢸⣿⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄
+⠘⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾⠃⠄
+⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃⠄
+⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⠄⣿⣿⡀⣾⣿⣿⣿⣿⣛⠛⠁⠄⠄
+⠄⠄⠄⠄⠈⠛⢿⣿⣿⣿⠁⠞⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁
+
+
+
+`
+}
+exports.help = help
